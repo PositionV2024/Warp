7a539aba2cd7cb951566532213d0fb566ef9778e
Clarence <clarencekaiwen@gmail.com>
2024-09-01 01:52:57 +0800

    Remove mongo database

--- src/main/java/com/clarence/warp/Warp.java
+++ src/main/java/com/clarence/warp/Warp.java
@@ -1,14 +1,8 @@
 package com.clarence.warp;
 
 import com.clarence.ToolHelper.Configuration;
-import com.clarence.ToolHelper.Database;
-import com.clarence.ToolHelper.DatabaseEnums;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.technicjelle.UpdateChecker;
 import dev.shreyasayyengar.menuapi.menu.MenuManager;
-import org.bson.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,12 +10,6 @@ public final class Warp extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        MongoClient dbClient = Database.createDataBase();
-        MongoDatabase database = Database.getDatabase(dbClient, "sample_mflix");
-        MongoCollection<Document> coll = database.getCollection("movies");
-        Document data = coll.find().first();
-        System.out.println(data.getString(DatabaseEnums.TITLE.getName()));
-
         UpdateChecker updateChecker = new UpdateChecker("PositionV2024", "Warp", Bukkit.getPluginManager().getPlugin(this.getName()).getDescription().getVersion());
 
         new MenuManager(this);
